c9378cc81c585c216d5c352a0d73d098bd71ab7a
HankyeolKim <pinky0927@naver.com>
2024-02-19 00:49:41 +0900

    Update : 구글 스프레드 시트 연동 구글 스프레드 시트를 통해 데이터 받아옴

--- Assets/Scripts/Data/GoogleSheetTest.cs
+++ Assets/Scripts/Data/GoogleSheetTest.cs
@@ -5,11 +5,11 @@ using UnityEngine.Networking;
 
 public class GoogleSheetTest : MonoBehaviour
 {
-    //const string URL = "https://docs.google.com/spreadsheets/d/1xGNPbePoOnsaKqLi-TXz-PoukBWAYeE_O_enQ0k_cxk/export?format=tsv&range=A2:B";
     public readonly string ADDRESS = "https://docs.google.com/spreadsheets/d/1xGNPbePoOnsaKqLi-TXz-PoukBWAYeE_O_enQ0k_cxk";
-    public readonly string RANGE = "A2:B";
+    public readonly string RANGE = "B2:D";
     public readonly long SHEET_ID = 0;
 
+    public TestSO testSO;
 
     IEnumerator Start() 
     {
@@ -19,11 +19,35 @@ public class GoogleSheetTest : MonoBehaviour
         yield return www.SendWebRequest();
 
         string data = www.downloadHandler.text;
-        Debug.Log(data);
+        //Debug.Log(data);
+        SetSO(data);
     }
 
     public static string GetTSVAdress(string address, string range, long sheedID) 
     {
         return $"{address}/export?format=tsv&range={range}&gid={sheedID}";
+    }
+
+    private void SetSO(string tsv)
+    {
+        string[] row = tsv.Split('\n');
+        int rowSize = row.Length;
+        int columnSize = row[0].Split('\t').Length;
+
+        for(int i = 0; i < rowSize; i++)
+        {
+            string[] column = row[i].Split('\t');
+            for(int j = 0; j < columnSize; j++)
+            {
+                if(testSO != null) 
+                {
+                    Stats stat = testSO.statsArray[i];
+
+                    stat.name = column[0];
+                    stat.birthday = int.Parse(column[1]);
+                    stat.sex = column[2];
+                }
+            }
+        }
     }
 }
