263176b89b41383a7a80d2759af4bcc2f2eb1316
SeokBeomKo <enngo98@gmail.com>
2024-02-18 22:10:55 +0900

    Fix : 맵 생성기 맵 생성기 리스트 깊은 복사 후 사용

--- Assets/Scripts/DungeonLevel/RoomGenerator.cs
+++ Assets/Scripts/DungeonLevel/RoomGenerator.cs
@@ -7,11 +7,14 @@ public class RoomGenerator : MonoBehaviour
 {
     [SerializeField] public List<RoomList> roomLists;
     [SerializeField] public Transform roomsParent;
+    private List<RoomList> copyLists;
 
     int gridSizeX, gridSizeY, tempGridX, tempGridY;
 
     public void CreateRoom(RoomData[,] rooms, List<Vector2> takenPositions, Vector2 worldSize)
     {
+        ListCopy();
+
         gridSizeX = Mathf.RoundToInt(worldSize.x); // 그리드의 가로 크기
         gridSizeY = Mathf.RoundToInt(worldSize.y); // 그리드의 세로 크기
 
@@ -23,16 +26,22 @@ public class RoomGenerator : MonoBehaviour
             if (rooms[tempGridX, tempGridY] == null)  return;
 
             Instantiate(SelectRoom(rooms[tempGridX, tempGridY]), new Vector3((int)takenPositions[i].x * 40, (int)takenPositions[i].y * 40), transform.rotation, roomsParent);
-
-            Debug.Log(rooms[tempGridX, tempGridY].direction);
         }
     }
 
     private GameObject SelectRoom(RoomData room)
     {
-        int random = Random.Range(0, roomLists[(int)room.direction].room.Count);
-        GameObject selectedRoom = roomLists[(int)room.direction].room[random];
-        roomLists[(int)room.direction].room.RemoveAt(random);
+        int random = Random.Range(0, copyLists[(int)room.direction].room.Count);
+        GameObject selectedRoom = copyLists[(int)room.direction].room[random];
+        copyLists[(int)room.direction].room.RemoveAt(random);
         return selectedRoom;
+    }
+
+    private void ListCopy()
+    {
+        copyLists = roomLists.ConvertAll(roomList => 
+            new RoomList { 
+                room = new List<GameObject>(roomList.room) 
+            });
     }
 }
