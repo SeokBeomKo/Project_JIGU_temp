7a59c31587dabe7ecb094836107dab25335c6f1f
HankyeolKim <pinky0927@naver.com>
2024-02-19 00:51:11 +0900

    Update : 구글 스프레드 시트 연동 구글 스프레드 시트를 통해 데이터 받아옴

---
+++ Assets/Scripts/Data/SO/TestSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stats
+{
+    public string name;
+    public int birthday;
+    public string sex;
+}
+
+[CreateAssetMenu(fileName = "TestSo", menuName = "scriptableObject/TestSo")]
+public class TestSO : ScriptableObject
+{
+    public Stats[] statsArray;
+}
--- Assets/Scripts/Player/Scriptable Object/FutureGirlStats.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "FutureGirl", menuName = "Players/FutureGirl")]
-public class FutureGirlStats : PlayerStats
-{
-    
-}
